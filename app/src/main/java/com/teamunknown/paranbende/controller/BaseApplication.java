d57558931ca86fe85ef7c1c63f0a6ddc24f9fdcf
halitogunc <hltognc@gmail.com>
2018-02-17 08:34:02 +0300

    Add Base Application

--- app/src/main/java/com/teamunknown/paranbende/controller/BaseApplication.java
+++ app/src/main/java/com/teamunknown/paranbende/controller/BaseApplication.java
@@ -1,8 +1,22 @@
 package com.teamunknown.paranbende.controller;
 
+import android.app.Application;
+import android.content.Context;
+
 /**
  * Created by halitogunc on 17.02.2018.
  */
 
-public class BaseApplication {
+public class BaseApplication extends Application {
+
+    private static Context context;
+    public static Context getAppContext(){
+        return BaseApplication.context;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
 }
