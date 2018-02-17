73de6e124bbf82e3094a39116482e60dc96a33ad
halitogunc <hltognc@gmail.com>
2018-02-17 13:27:47 +0300

    Add Data Model

---
+++ app/src/main/java/com/teamunknown/paranbende/model/Data.java
@@ -0,0 +1,33 @@
+package com.teamunknown.paranbende.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by halitogunc on 17.02.2018.
+ */
+
+public class Data {
+    @SerializedName("user")
+    @Expose
+    private User user;
+    @SerializedName("token")
+    @Expose
+    private String token;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
